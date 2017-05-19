381a33915425ebd7b1507f59b7f8a15b0bdf1b70
BendingUnit-22 <wur1@wit.edu>
2017-05-19 11:27:46 -0400

    added layout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// <img src={logo} className="App-logo" alt="logo" />
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
